a5aaa0e5a09b0fb46f83eaf35c7acf2f2eb932fd
Lakshmi Rajendran <lakshmitanhars@gmail.com>
2025-05-24 14:52:01 +0100

    Create Print_1to100.py

---
+++ Print_1to100.py
@@ -0,0 +1,14 @@
+# 1. Print numbers from 1 to 100 using a for loop
+
+# using for loop
+
+for i in range(1,101):
+    print(i)
+
+#  using While loop
+
+i=1
+while (i<=100):
+     print(i)
+     i+=1
+
