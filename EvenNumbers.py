74aa89c99f8b12ac94e54ca18e356bfb5f344cd1
Lakshmi Rajendran <lakshmitanhars@gmail.com>
2025-05-24 14:52:53 +0100

    Create EvenNumbers.py

---
+++ EvenNumbers.py
@@ -0,0 +1,12 @@
+# 2. Print even numbers from 1 to 50 using a while loop
+
+for i in range(1, 51):
+    if(i%2 == 0):
+        print(i)
+
+print("Using While Loop Print Even Number till 50")
+i=1
+while (i<=50):
+    if i%2 ==0:
+        print(i)
+    i+=1
