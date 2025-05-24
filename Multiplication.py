dcd6e8429b14e53800dc6003142cab139ff35134
Lakshmi Rajendran <lakshmitanhars@gmail.com>
2025-05-24 14:54:25 +0100

    Create Multiplication.py

---
+++ Multiplication.py
@@ -0,0 +1,18 @@
+# 3. Multiplication table using a for loop
+
+for i in range(0,100,5):
+        print(i)
+
+for i in range(100,0,-5):
+        print(i)
+# Using While Loop
+i =0
+while(i<=100):
+    print(i)
+    i+=5
+
+
+i=100
+while(i>=1):
+    print(i)
+    i=i-5
